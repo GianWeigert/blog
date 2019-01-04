cb8522f318ddd27dc489f3cba4160befcba17138
Gian Weigert <gian.rodrigues@hotmail.com>
2019-01-04 20:37:27 -0200

    Edit category
    The edit category function was created. It was developed the view
    edit category.

--- routes/admin.js
+++ routes/admin.js
@@ -10,10 +10,6 @@ router.get('/', function(request, response) {
     return response.render('admin/index');
 });
 
-router.get('/posts', function(request, response) {
-
-});
-
 router.get('/categories', function(request, response) {
     Category.find().then((categories) => {
         return response.render('admin/categories/list_categories', {categories: categories});
@@ -47,6 +43,44 @@ router.post('/categories/create', function(request, response) {
         return response.redirect('/admin/categories');
     }).catch((error) => {
         console.log('Error saving category: ' + error);
+    });
+});
+
+router.get('/categories/edit/:id', function(request, response) {
+    Category.findById(request.params.id).then((category) => {
+        return response.render('admin/categories/edit_category', {category: category});
+    }).catch((error) => {
+        console.log('Error Category not found: ' + error);
+    });
+});
+
+router.post('/categories/edit/:id', function(request, response) {
+    Category.findById(request.params.id).then((category) => {
+        var formParameters = {
+            name: request.body.name,
+            slug: request.body.slug,
+            description: request.body.description
+        }
+
+        var validator =  new Validator(formParameters, CategoryValidator);
+        validator.validate();
+
+        if (!validator.isValid()) {
+            var errors = validator.getErrors();
+            return response.render('admin/categories/edit_category', {category: category, errors: errors});
+        }
+
+        category.name = request.body.name;
+        category.slug = request.body.slug;
+        category.description = request.body.description;
+
+        category.save().then(() => {
+            return response.redirect('/admin/categories');
+        }).catch((error) => {
+            console.log('Error saving category: ' + error);
+        });
+    }).catch((error) => {
+        console.log('Error to edit category: Category not found: ' + error);
     });
 });
 
