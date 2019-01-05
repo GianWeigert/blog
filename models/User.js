d69534589ef353c97c0f13713c294d2f0824b915
Gian Weigert <gian.rodrigues@hotmail.com>
2019-01-04 23:03:09 -0200

    User model
    The model user was created to manipulate users data.

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+mongoose.model('users', User);
